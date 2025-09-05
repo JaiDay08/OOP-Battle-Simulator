b7614e9bc0eba44de779c9c7f29c1087a22560b5
JaiDay08 <jaida.day@ascte.org>
2025-09-05 12:36:00 -0500

    connecting to parent file

--- goblin.py
+++ goblin.py
@@ -1,4 +1,6 @@
 import random
+from enemy import Enemy 
+
 
 class Goblin:
     """
@@ -9,20 +11,9 @@ class Goblin:
         health: The current health value 
         attack_power: How much health will be drained from opponent if hit
     """
-    def __init__(self, name):
-        self.name = name
-        self.health = 100
-        self.attack_power = random.randint(5, 15)
+    class Goblin(Enemy): 
+        def __init__(self, name, color):
+            super()._init__(name)
+            self.color = color
 
-    def attack(self):
-        return random.randint(1, self.attack_power)
 
-    def take_damage(self, damage):
-        self.health -= damage
-        if self.health < 0: 
-            self.health = 0
-            print(f"{self.name} is dead!")
-        print(f"{self.name} takes {damage} damage. Health is now {self.health}.")
-
-    def is_alive(self):
-        return self.health > 0
